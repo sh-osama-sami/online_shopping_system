9d2608ae26ce570f00d697ba4a39af2c54e829ea
ibrahim Soltan <ibrahimsoltan2000@gmail.com>
2023-06-11 18:52:37 +0300

    final with local host

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 
 const sessionId = Cookies.get('JSESSIONID');
 export const axiosClient = axios.create({
-    baseURL: 'https://customerservice-lepf.onrender.com/',
+    baseURL: 'http://127.0.0.1:4000/',
     headers: {
         'Content-type': 'application/json',
          Cookie: `JSESSIONID=${sessionId}`
@@ -14,7 +14,7 @@ export const axiosClient = axios.create({
     
 });
 export const axiosSelling = axios.create({
-    baseURL: 'https://8a75-197-39-201-230.ngrok-free.app/AdminService-1.0-SNAPSHOT/api',
+    baseURL: 'http://127.0.0.1:8080/AdminService-1.0-SNAPSHOT/api',
     headers: { 'Content-Type': 'application/json' ,
     Cookie: `JSESSIONID=${sessionId}`
 },
